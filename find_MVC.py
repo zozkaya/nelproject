78daa834d937d847ff13e8461389e78f412fc586
Lauren Parola <88797808+lrparola@users.noreply.github.com>
2023-11-28 09:33:59 -0500

    Add files via upload

---
+++ find_MVC.py
@@ -0,0 +1,20 @@
+## file for classifier code 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import os
+
+def calculate_MVC(path):
+    trial_dict = {}
+    #input: file path of the MVCs
+    #return: dictionary of max contract value for each channel and each task
+    for trial in os.listdir(path):
+        #extract only file ending in .txt (for if on mac)
+        if '.txt' in trial:
+            #reach in file
+            data=pd.read_csv(os.path.join(path,trial), header=0,sep=',',skiprows=4)
+            #find maximum
+            trial_dict[trial.replace('.txt','')] = data[[col for col in data if 'EXG' in col]]).max()
+            
+    return trial_dict
