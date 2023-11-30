c1e6f5cb3f757c54bf1e94316a3fc5c85af80c71
Zeynep Ozkaya <zeynepozkaya@Zeyneps-MacBook-Pro-2.local>
2023-11-29 19:50:29 -0500

    erging

--- find_MVC.py
+++ find_MVC.py
@@ -5,11 +5,6 @@ import pandas as pd
 import math
 import os
 
-<<<<<<< HEAD
-=======
-path = '/Users/zeynepozkaya/Desktop/MVC'
-
->>>>>>> cc9e6b4a994a595bcd2594fed6ea3a1dfd7542aa
 def calculate_MVC(path):
     trial_dict = {}
     #input: file path of the MVCs
@@ -19,16 +14,7 @@ def calculate_MVC(path):
         if '.txt' in trial:
             #reach in file
             data=pd.read_csv(os.path.join(path,trial), header=0,sep=',',skiprows=4)
-<<<<<<< HEAD
             #find maximum
             trial_dict[trial.replace('.txt','')] = data[[col for col in data if 'EXG' in col]].max()
             
     return trial_dict
-=======
-
-            #find maximum
-            trial_dict[trial.replace('.txt','')] = data[[col for col in data if 'EXG' in col]].iloc[50:].max()
-    
-    return trial_dict
-
->>>>>>> cc9e6b4a994a595bcd2594fed6ea3a1dfd7542aa
